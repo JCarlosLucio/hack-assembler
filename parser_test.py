f6bf1ab2d6d33feecab4857747d2e391f65b554d
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-10-28 17:52:06 -0500

    Test parsing with inline comments

--- parser_test.py
+++ parser_test.py
@@ -5,3 +5,29 @@ def test_simple_parse():
     with open("./to_assemble/Add.asm") as file:
         simple_text = file.read()
     assert parse(simple_text) == ["@2", "D=A", "@3", "D=D+A", "@0", "M=D"]
+
+
+def test_parse_with_inline_comments():
+    with open("./to_assemble/Max.asm") as file:
+        with_inline_comments = file.read()
+    assert parse(with_inline_comments) == [
+        "@R0",
+        "D=M",
+        "@R1",
+        "D=D-M",
+        "@OUTPUT_FIRST",
+        "D;JGT",
+        "@R1",
+        "D=M",
+        "@OUTPUT_D",
+        "0;JMP",
+        "(OUTPUT_FIRST)",
+        "@R0",
+        "D=M",
+        "(OUTPUT_D)",
+        "@R2",
+        "M=D",
+        "(INFINITE_LOOP)",
+        "@INFINITE_LOOP",
+        "0;JMP",
+    ]
